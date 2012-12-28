fcb59090662c6bda281e5924e9ef433c8a48586f
Jose Prous <joseprous@gmail.com>
2012-12-28 16:01:26 -0300

    call odb from cmake

--- driver.cpp
+++ driver.cpp
@@ -1,4 +1,3 @@
-#include <memory>   // std::auto_ptr
 #include <iostream>
 
 #include <odb/database.hxx>
@@ -13,48 +12,55 @@
 using namespace std;
 using namespace odb::core;
 
+void create_person_table(odb::sqlite::database& db)
+{
+    unsigned long john_id, jane_id, joe_id;
+
+    // Create a few persistent person objects.
+    //
+
+    person john ("John", "Doe", 33);
+    person jane ("Jane", "Doe", 32);
+    person joe ("Joe", "Dirt", 30);
+
+    {
+        transaction t (db.begin());
+
+        odb::schema_catalog::create_schema(db);
+
+        // Make objects persistent and save their ids for later use.
+        //
+        john_id = db.persist (john);
+        jane_id = db.persist (jane);
+        joe_id = db.persist (joe);
+        
+        t.commit ();
+    }    
+}
+
+void query_person(odb::sqlite::database& db)
+{
+    typedef odb::query<person> query;
+
+    transaction t (db.begin());
+            
+    auto r (db.query<person>(query::age > 30));
+
+    for (auto i:r){
+        cout << "Hello, " << i.first() << "!" << endl;
+    }
+
+    t.commit ();
+}
+
 int main (int argc, char* argv[])
 {
     try{
         odb::sqlite::database db ("test.db",
                                   SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
                 
-        unsigned long john_id, jane_id, joe_id;
-
-        // Create a few persistent person objects.
-        //
-
-        person john ("John", "Doe", 33);
-        person jane ("Jane", "Doe", 32);
-        person joe ("Joe", "Dirt", 30);
-        {
-            transaction t (db.begin());
-
-            odb::schema_catalog::create_schema(db);
-
-            // Make objects persistent and save their ids for later use.
-            //
-            john_id = db.persist (john);
-            jane_id = db.persist (jane);
-            joe_id = db.persist (joe);
-            
-            t.commit ();
-        }
-
-typedef odb::query<person> query;
-typedef odb::result<person> result;
-        {
-            transaction t (db.begin());
-            
-            result r (db.query<person>(query::age > 30));
-
-            for (auto i:r){
-                cout << "Hello, " << i.first() << "!" << endl;
-            }
-
-            t.commit ();
-        }
-
+        create_person_table(db);
+        query_person(db);
     }
     catch (const odb::exception& e){
         cerr << e.what () << endl;
